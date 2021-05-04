96b504825c715e0192b23a8b7a8665fe660e12e8
전재일 <60211008+JeonBest@users.noreply.github.com>
2021-05-05 08:42:08 +0900

    button image add

--- App/AppDevelopment/InitialViewController.swift
+++ App/AppDevelopment/InitialViewController.swift
@@ -2,21 +2,24 @@
 //  ViewController.swift
 //  DreamDream
 //
-//  Created by 원석 on 2021/04/08.
-//
 
 import UIKit
 
 class InitialViewController: UIViewController {
 
+    @IBOutlet weak var dreamIconButton: UIButton!
+    
     @IBAction func SegueSwipeUp(sender: UISwipeGestureRecognizer) {
         self.performSegue(withIdentifier: "toSettings", sender: self)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationController?.interactivePopGestureRecognizer?.delegate = nil
+        
+        
     }
-
+    
 
 }
 
