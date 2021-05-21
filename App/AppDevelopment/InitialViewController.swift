45ac9108b7f458814c8824ff3caff0f0b3c90cb1
이원석 <wonseok@iwonseog-ui-MacBookPro-2.local>
2021-05-22 02:11:22 +0900

    Imported HealthKit and made InitialView Prototype, skeletone code for sleep analysis

--- App/AppDevelopment/InitialViewController.swift
+++ App/AppDevelopment/InitialViewController.swift
@@ -4,19 +4,67 @@
 //
 
 import UIKit
+import HealthKit
 
 class InitialViewController: UIViewController {
-
+    public var startTime = TimeInterval()
+    public var timer:Timer = Timer()
+    public var endTime: NSDate!
+    public var alarmTime: NSDate!
+    public let healthStore = HKHealthStore()
+    
+    @IBOutlet weak var displayTimeLabel: UILabel!
+    
     @IBOutlet weak var dreamIconButton: UIButton!
 
+    @IBOutlet weak var currentTimeLabel: UILabel!
     override func viewDidLoad() {
+        let date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "HH:mm:ss"
+        let formattedDate = format.string(from: date)
+        currentTimeLabel.text = formattedDate
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         navigationController?.interactivePopGestureRecognizer?.delegate = nil
         
         
     }
+    @IBAction func startButtonTapped(_ sender: Any) {
+        alarmTime = NSDate()
+        if (!timer.isValid) {
+            let aSelector : Selector = #selector(InitialViewController.updateTime)
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+            startTime = NSDate.timeIntervalSinceReferenceDate
+        }
+    }
     
-
+    @objc func updateTime() {
+        let currentTime = NSDate.timeIntervalSinceReferenceDate
+        
+        //Find the difference between current time and start time.
+        var elapsedTime: TimeInterval = currentTime - startTime
+        let hours = UInt8(elapsedTime / 3600)
+        
+        //calculate the minutes in elapsed time.
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (TimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= TimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        //let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        let setHours = String(format: "%02d", hours)
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        //let strFraction = String(format: "%02d", fraction)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        displayTimeLabel.text = "\(setHours):\(strMinutes):\(strSeconds)"
+    }
 }
 
--- App/AppDevelopment/SleepAnalysysViewController.swift
+++ App/AppDevelopment/SleepAnalysysViewController.swift
@@ -7,25 +7,139 @@
 
 import UIKit
 import HealthKit
-class SleepAnalysysViewController: UIViewController {
 
+   
+
+
+class SleepAnalysysViewController: UIViewController{
+    
     @IBOutlet weak var homeButton: UIButton!
+    
+
+    @IBOutlet var displayTimeLabel: UILabel!
+    
+    var startTime = TimeInterval()
+    var timer:Timer = Timer()
+    var endTime: NSDate!
+    var alarmTime: NSDate!
+    let healthStore = HKHealthStore()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let typestoRead = Set([
+            HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
+                ])
+        
+            let typestoShare = Set([
+                HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
+                ])
+        
+        
+            
+        self.healthStore.requestAuthorization(toShare: typestoShare, read: typestoRead) { (success, error) -> Void in
+                if success == false {
+                    NSLog(" Display not allowed")
+                }
+    }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // 아래 함수는 화면을 다시 켰을때, 즉 잠금모드 이후에 어플을 다시 활성화 시에 등록
+    func stop(sender: AnyObject) {
+        endTime = NSDate()
+        saveSleepAnalysis()
+        retrieveSleepAnalysis()
+        timer.invalidate()
     }
-    */
+    
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func retrieveSleepAnalysis() {
+        
+        // first, we define the object type we want
+        if let sleepType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) {
+            print(sleepType)
+            // Use a sortDescriptor to get the recent data first
+            let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+            
+            // we create our query with a block completion to execute
+            let query = HKSampleQuery(sampleType: sleepType, predicate: nil, limit: 30, sortDescriptors: [sortDescriptor]) { (query, tmpResult, error) -> Void in
+                
+                if error != nil {
+                    
+                    // something happened
+                    return
+                    
+                }
+                
+                if let result = tmpResult {
+                    
+                    // do something with my data
+                    for item in result {
+                        if let sample = item as? HKCategorySample {
+                            let value = (sample.value == HKCategoryValueSleepAnalysis.inBed.rawValue) ? "InBed" : "Asleep"
+                            print("Healthkit sleep: \(sample.startDate) \(sample.endDate) - value: \(value)")
+                        }
+                    }
+                }
+            }
+            
+            // finally, we execute our query
+            healthStore.execute(query)
+        }
+    }
+    func saveSleepAnalysis() {
+        
+        // alarmTime and endTime are NSDate objects
+        if let sleepType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) {
+            
+            // we create our new object we want to push in Health app
+            let object = HKCategorySample(type:sleepType, value: HKCategoryValueSleepAnalysis.inBed.rawValue, start: self.alarmTime as Date, end: self.endTime as Date)
+            
+            // at the end, we save it
+            healthStore.save(object, withCompletion: { (success, error) -> Void in
+                
+                if error != nil {
+                    // something happened
+                    return
+                }
+                
+                if success {
+                    print("My new data was saved in HealthKit")
+                    
+                } else {
+                    // something happened again
+                }
+                
+            })
+            
+            
+            let object2 = HKCategorySample(type:sleepType, value: HKCategoryValueSleepAnalysis.asleep.rawValue, start: self.alarmTime as Date, end: self.endTime as Date)
+            
+            healthStore.save(object2, withCompletion: { (success, error) -> Void in
+                if error != nil {
+                    // something happened
+                    return
+                }
+                
+                if success {
+                    print("My new data (2) was saved in HealthKit")
+                } else {
+                    // something happened again
+                }
+                
+            })
+            
+        }
+        
+    }
 }
+    
+
+
