07a3b35ce47e1b99390a467ae2658387445cb992
전재일 <60211008+JeonBest@users.noreply.github.com>
2021-06-03 18:44:27 +0900

    youtube toy

---
+++ Sandbox/YoutubeToy/YoutubeToy/ViewController.swift
@@ -0,0 +1,42 @@
+import UIKit
+import Kingfisher
+
+class Video {
+    var Key: String = ""
+    var Title: String = ""
+}
+
+class ViewController: UIViewController {
+    var videos:[Video] = []
+    @IBOutlet weak var Thumnail1: UIImageView!
+    @IBOutlet weak var Thumnail2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let video1 = Video()
+        video1.Key = "V2NWKmyHk_Q"
+        video1.Title = "월요병? 그게뭔데 ㅋ 듣다보면 저절로 어깨가 들석거리는 팝송 모음"
+        videos.append(video1)
+
+        let video2 = Video()
+        video2.Key = "8r3ySJvEOCM"
+        video2.Title = "듣고만 있어도 기분 좋아지는 노래모음"
+        videos.append(video2)
+        
+        load()
+    }
+
+    func load() {
+        
+        let urlString = "https://img.youtube.com/vi/\(videos[0].Key)/0.jpg"
+        let fileURL = URL(string: urlString)
+        Thumnail1.kf.setImage(with: fileURL)
+        let urlString2 = "https://img.youtube.com/vi/\(videos[1].Key)/0.jpg"
+        let fileURL2 = URL(string: urlString2)
+        Thumnail2.kf.setImage(with: fileURL2)
+        
+    }
+}
+
